667566f71ba4de482cc43572e2d9d5e6e2656b05
hzdong <1065900847@qq.com>
2020-08-11 18:16:02 +0800

    day1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+//引入组件
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+export default {
+  name: "App",
+  components: {
+    //注册组件
+    Header,
+    Footer
+  }
+};
+</script>
+
+<style>
+</style>
